401510d860bf2188043b0589bc05d79e371a178d
Hugo Jobling <hello@thisishugo.com>
2018-07-10 01:31:21 +0100

    Yet another refresh

--- js/thisishugo.js
+++ js/thisishugo.js
@@ -3,6 +3,6 @@ for (const el of document.querySelectorAll('.email')) {
   const link = document.createElement('a')
   link.href = 'mailto:' + href
   link.innerHTML = href
-  link.classList.add('link')
+  link.classList.add('email')
   el.parentNode.replaceChild(link, el)
 }
