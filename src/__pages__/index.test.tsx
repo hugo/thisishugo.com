c5b928d19547a636b277b66f98ffe5866df2b798
Hugo Jobling <hello@thisishugo.com>
2020-02-11 13:12:08 +0000

    Move tests to `src/__tests__`

--- src/__pages__/index.test.tsx
+++ src/__pages__/index.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import React from "react";
 
-import Index from "../../pages";
+import Index from "../pages/index";
 
 describe("<Index />", () => {
   test("welcome message", () => {
