542f037a4ece02382e61d8c767432d667c1944d6
Hugo Jobling <hello@thisishugo.com>
2020-02-11 12:26:25 +0000

    Remove unnededed `<div />` wrapper

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,44 +4,39 @@ import largeImage from "../images/hugo_large.jpg";
 import ogImage from "../images/hugo_og.jpg";
 
 export default () => (
-  <div>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta
-        content="width=device-width, initial-scale=1, viewport-fit=cover"
-        name="viewport"
-      />
+  <Head>
+    <meta charSet="utf-8" />
+    <meta
+      content="width=device-width, initial-scale=1, viewport-fit=cover"
+      name="viewport"
+    />
 
-      <title>Hugo Jobling</title>
+    <title>Hugo Jobling</title>
 
-      <meta
-        content="Hugo Jobling. A programmer, writer, and human"
-        name="description"
-      />
+    <meta
+      content="Hugo Jobling. A programmer, writer, and human"
+      name="description"
+    />
 
-      {/* Facebook */}
-      <meta content="Hugo Jobling" property="og:title" />
-      <meta content="Hugo Jobling" property="og:site_name" />
-      <meta content="https://thisishugo.com" property="og:url" />
-      <meta
-        content="Hugo Jobling. A programmer, writer, and human"
-        property="og:description"
-      />
-      <meta content="profile" property="og:type" />
-      <meta content={ogImage} property="og:image" />
+    {/* Facebook */}
+    <meta content="Hugo Jobling" property="og:title" />
+    <meta content="Hugo Jobling" property="og:site_name" />
+    <meta content="https://thisishugo.com" property="og:url" />
+    <meta
+      content="Hugo Jobling. A programmer, writer, and human"
+      property="og:description"
+    />
+    <meta content="profile" property="og:type" />
+    <meta content={ogImage} property="og:image" />
 
-      {/* Twitter */}
-      <meta content="summary" name="twitter:card" />
-      <meta content="@dissimile" name="twitter:site" />
-      <meta content="Hugo Jobling" name="twitter:title" />
-      <meta
-        content="Programmer, writer, and human"
-        name="twitter:description"
-      />
-      <meta content={largeImage} name="twitter:image" />
+    {/* Twitter */}
+    <meta content="summary" name="twitter:card" />
+    <meta content="@dissimile" name="twitter:site" />
+    <meta content="Hugo Jobling" name="twitter:title" />
+    <meta content="Programmer, writer, and human" name="twitter:description" />
+    <meta content={largeImage} name="twitter:image" />
 
-      <link href="/icons/favicon.ico" rel="shortcut icon" />
-      <link href="/icons/apple-touch-icon.png" rel="apple-touch-icon" />
-    </Head>
-  </div>
+    <link href="/icons/favicon.ico" rel="shortcut icon" />
+    <link href="/icons/apple-touch-icon.png" rel="apple-touch-icon" />
+  </Head>
 );
