2e9600822d57980ad1177f837ffb6ff8f6f0d948
Hugo Jobling <hello@thisishugo.com>
2021-03-23 20:55:39 +0000

    Use updated prettier ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'prettier/react',
+    'prettier',
   ],
   settings: {
     react: {
