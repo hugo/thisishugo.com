a9ce568d0dd7f6ecd1d031acefd6faf1a560cce8
Hugo Jobling <hello@thisishugo.com>
2021-06-01 23:16:39 +0100

    Use Remix server instead of custom Express

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,28 @@
 import ReactDOMServer from 'react-dom/server'
 import {RemixServer as Remix, EntryContext, redirect} from 'remix'
 
+let csp =
+  process.env.NODE_ENV === 'production'
+    ? [
+        "default-src 'none'",
+        "script-src 'self' 'unsafe-inline' https:",
+        "script-src-elem 'self' 'unsafe-inline' https:",
+        "style-src 'self'",
+        "style-src-elem 'self'",
+        "img-src 'self' data:",
+        "require-trusted-types-for 'script'",
+      ].join('; ')
+    : [
+        "default-src 'none'",
+        "script-src 'unsafe-inline' http://localhost:3000",
+        "script-src-elem 'unsafe-inline' http://localhost:3000",
+        "style-src 'self'",
+        "style-src-elem 'self' http://localhost:3000",
+        "img-src 'self' data:",
+        'connect-src ws://localhost:3001',
+        "require-trusted-types-for 'script'",
+      ].join('; ')
+
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
@@ -12,7 +34,12 @@ export default function handleRequest(
   if (process.env.NODE_ENV === 'production' && proto !== 'https') {
     let url = new URL(request.url)
     url.protocol = 'https'
-    return redirect(url.toString(), 307)
+    return redirect(url.toString(), {
+      status: 307,
+      headers: {
+        'X-Powered-By': 'gremlins',
+      },
+    })
   }
 
   let markup = ReactDOMServer.renderToString(
@@ -21,6 +48,12 @@ export default function handleRequest(
 
   let headers = new Headers(responseHeaders)
   headers.set('Content-Type', 'text/html')
+  headers.set('X-Powered-By', 'gremlins')
+  headers.set(
+    'Strict-Transport-Security',
+    'max-age=63072000; includeSubDomains; preload'
+  )
+  headers.set('Content-Security-Policy', csp)
 
   return new Response('<!DOCTYPE html>' + markup, {
     status: responseStatusCode,
