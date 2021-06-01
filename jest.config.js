84e7c0b90ff07cd49a432b4c7dad85fbc6f5280c
Hugo Jobling <hello@thisishugo.com>
2021-06-02 00:20:43 +0100

    Explicitly specify "jsdom" `testEnvironment`
    As of Jest 27 this is necessary as the default changes to "node" from
    "jsdom".

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tests/fileTransformer.js',
   },
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
   globals: {
     'ts-jest': {
