2ff5459265a4af299c5cd3d2012fd28c72230d69
Hugo Jobling <hello@thisishugo.com>
2022-09-20 10:44:32 +0100

    Import `<Outlet />` from "@remix/react"
    Using the "react-router-dom" import results in an empty page.

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,5 @@
 import type {LinksFunction, MetaFunction} from '@remix-run/node'
-import {Links, LiveReload, Meta, Scripts} from '@remix-run/react'
-import {Outlet} from 'react-router-dom'
+import {Links, LiveReload, Meta, Outlet, Scripts} from '@remix-run/react'
 
 import styles from './styles/index.css'
 
