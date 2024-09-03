9f8a69ef73e634d4f4a5b5a2ba588fe94ccec351
Hugo Jobling <hello@thisishugo.com>
2024-09-04 00:22:08 +0100

    Revamp homepage yet again
    Remove the Medium and Twitter links - who cares?
    
    Tweak the text sizes.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type {MetaFunction} from '@remix-run/node'
 import type {AnchorHTMLAttributes} from 'react'
-import {twJoin} from 'tailwind-merge'
+import {twMerge} from 'tailwind-merge'
 
 export let meta: MetaFunction = ({matches}) => [
   ...matches.flatMap(({meta = []}) => meta),
@@ -9,10 +9,14 @@ export let meta: MetaFunction = ({matches}) => [
 
 export default function Index() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center overflow-hidden p-4">
+    <div className="flex flex-1 flex-col items-center justify-center overflow-hidden">
       <div className="overflow-y-auto rounded border-2 border-solid border-gray-700">
         <div className="bg-gray-100">
-          <div className="flex flex-col items-center justify-around gap-4 p-4">
+          <div className="flex flex-col items-center justify-around gap-2 p-2">
+            <div className="flex flex-col items-center gap-1">
+              <p className="text-md">Hello, this is Hugo</p>
+            </div>
+
             <img
               src="/images/hugo.jpg"
               className="rounded-full border-2 border-solid border-gray-700"
@@ -23,24 +27,12 @@ export default function Index() {
 
             <a
               href="mailto:hello@thisishugo.com"
-              className="text-sm text-pink-400 underline transition-colors hover:text-pink-500 md:text-2xl"
+              className="text-lg text-gray-800 underline transition-colors hover:text-gray-500"
             >
               hello@thisishugo.com
             </a>
 
             <div className="flex w-full flex-col items-center justify-center gap-2">
-              <Link
-                className="border-twitter text-twitter hover:bg-twitter hover:text-white"
-                href="https://twitter.com/dissimile"
-              >
-                Twitter
-              </Link>
-              <Link
-                className="border-medium p-1 text-medium hover:bg-medium hover:text-white"
-                href="https://words.thisishugo.com"
-              >
-                Medium
-              </Link>
               <Link
                 className="border-black text-black hover:bg-black hover:text-white"
                 href="https://github.com/hugo"
@@ -62,8 +54,8 @@ function Link({
 }: AnchorHTMLAttributes<HTMLAnchorElement>) {
   return (
     <a
-      className={twJoin(
-        'block w-full rounded border-2 border-solid p-1 text-center text-sm transition-colors md:text-lg',
+      className={twMerge(
+        'block w-full rounded border-2 border-solid p-1 text-center text-xs transition-colors',
         className
       )}
       {...props}
