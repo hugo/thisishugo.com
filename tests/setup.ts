d135eb3aa58574a09721e9576ac5bdda6c3f101a
Hugo Jobling <hello@thisishugo.com>
2026-01-01 17:36:25 +0000

    Move to Vitest from Jest
    Better performance, and we already have Vite

---
+++ tests/setup.ts
@@ -0,0 +1,10 @@
+import {afterEach, expect} from 'vitest'
+import * as matchers from '@testing-library/jest-dom/matchers'
+import {cleanup} from '@testing-library/react'
+
+// Ensure DOM is cleaned between tests when globals are disabled
+afterEach(() => {
+  cleanup()
+})
+
+expect.extend(matchers)
