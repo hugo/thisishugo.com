47447a1419cd02ea37d1e9c0495cc3612eb7051c
Hugo Jobling <hello@thisishugo.com>
2026-01-01 17:36:25 +0000

    Move to Vitest from Jest
    Better performance, and we already have Vite

--- tests/fileTransformer.js
+++
@@ -1,7 +0,0 @@
-import {basename} from 'node:path'
-
-export default {
-  process(_src, filename, _config, _options) {
-    return 'module.exports = ' + JSON.stringify(basename(filename)) + ';'
-  },
-}
