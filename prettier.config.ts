956a9968001771ac19c15493c05348f575dc8e1a
Hugo Jobling <hello@thisishugo.com>
2026-01-01 18:05:50 +0000

    Modernise fly.toml

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,8 +1,20 @@
 import type {Config} from 'prettier'
 import type {PluginOptions as TailwindPluginOptions} from 'prettier-plugin-tailwindcss'
+import type {prettierOptionsDefinitions} from 'prettier-plugin-toml'
+
+// Derive the options config from the available options.
+// Why this isn't already there beats me.
+type TomlPrettierOptionsDefinitions = typeof prettierOptionsDefinitions
+export type TomlPluginOptions = {
+  [K in keyof TomlPrettierOptionsDefinitions]?: TomlPrettierOptionsDefinitions[K]['type'] extends 'boolean'
+    ? boolean
+    : TomlPrettierOptionsDefinitions[K]['type'] extends 'int'
+      ? number
+      : unknown
+}
 
 export default {
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-toml'],
   printWidth: 80,
   tailwindStylesheet: './app/styles/index.css',
   overrides: [
@@ -15,6 +27,15 @@ export default {
         bracketSpacing: false,
         arrowParens: 'always',
       },
+    },
+    {
+      files: '*.toml',
+      options: {
+        alignEntries: false,
+        compactArrays: true,
+        indentEntries: true,
+        indentTables: true,
+      } satisfies TomlPluginOptions,
     },
     {
       files: '*.html',
